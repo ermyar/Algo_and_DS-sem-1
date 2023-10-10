50af329b4b0a5c9e2ea77cff60e32a3807f02043
yaroslav <eryar.school@gmail.com>
2023-10-10 13:48:20 +0300

    code changed after review

--- contest_1/1B.cpp
+++ contest_1/1B.cpp
@@ -1,29 +1,34 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
-const int kMaxn = 1e6 + 100, kEps = 6;
+const int kEps = 6;
 
-double ar[kMaxn];
-double pref[kMaxn];
+void DoQuest(std::vector<double>& pref) {
+  int lx;
+  int rx;
+  std::cin >> lx >> rx;
+  double val = pref[rx + 1] - pref[lx];
+  val /= (double)(rx - lx + 1);
+  double ans = std::exp(val);
+  std::cout << ans << std::endl;
+}
 
 int main() {
   std::cout << std::fixed << std::setprecision(kEps);
-  int nu;
-  std::cin >> nu;
-  for (int i = 0; i < nu; ++i) {
+  int number;
+  std::cin >> number;
+  std::vector<double> ar(number);
+  std::vector<double> pref(number + 1);
+  pref[0] = 0;
+  for (int i = 0; i < number; ++i) {
     std::cin >> ar[i];
     pref[i + 1] = pref[i] + std::log(ar[i]);
   }
-  int qu;
-  std::cin >> qu;
-  while ((qu--) != 0) {
-    int lx;
-    int rx;
-    std::cin >> lx >> rx;
-    double val = pref[rx + 1] - pref[lx];
-    val /= (double)(rx - lx + 1);
-    double ans = std::exp(val);
-    std::cout << ans << std::endl;
+  int quest_count;
+  std::cin >> quest_count;
+  for (int i = 0; i < quest_count; ++i) {
+    DoQuest(pref);
   }
 }
